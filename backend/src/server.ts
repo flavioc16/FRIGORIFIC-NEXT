1ad4b924da2ab62ef1304bc8a3bcc18151d7f229
Flavio Castro <flavio2008sousa@hotmail.com>
2024-12-07 10:07:35 -0300

    Commit automático

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,31 @@
 import cron from 'node-cron';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { router } from './routes';
+
+//functions
 import { aplicarJuros } from './jobs/aplicarJuros'; // Importando a função
 import { criarBackupJSON } from './jobs/criarBackupJSON';
 import { commitAutomatico } from './jobs/autoCommit';
 import { criarBackupSQL } from './jobs/criarBackupSQL';
 
-// Agendar para rodar a cada minuto
-cron.schedule('0 8 * * *', async () => {
-    console.log('Executando script de juros...');
-    await aplicarJuros(); // Chama a função de aplicar juros
+aplicarJuros();
+commitAutomatico();
+criarBackupSQL();
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('Executando script de juros...');
+  await aplicarJuros();
+});
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('Executando backup completo...');
+  await criarBackupSQL();
+});
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('Chamando a função de commit');
+  await commitAutomatico(); // Chama a função de criar backup
 });
 
 // cron.schedule('* * * * *', async () => {
@@ -15,21 +33,6 @@ cron.schedule('0 8 * * *', async () => {
 //   await criarBackupJSON(); // Chama a função de criar backup
 // });
 
-cron.schedule('* * * * *', async () => {
-  console.log('Chamando a função de commit');
-  await commitAutomatico(); // Chama a função de criar backup
-});
-
-cron.schedule('* * * * *', async () => {
-  console.log('Executando backup completo...');
-  await criarBackupSQL();
-});
-
-
-// Configuração do servidor Express
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import { router } from './routes';
 
 
 const app = express();
