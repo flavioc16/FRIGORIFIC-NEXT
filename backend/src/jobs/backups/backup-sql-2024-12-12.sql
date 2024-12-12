d08afbbc91a6ea1571f74b41a8377d0599cc9e40
Flavio Castro <flavio2008sousa@hotmail.com>
2024-12-12 11:55:00 -0300

    Commit automático para backup

--- backend/src/jobs/backups/backup-sql-2024-12-12.sql
+++ backend/src/jobs/backups/backup-sql-2024-12-12.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a3fedd5dd06783418f1a1283824e0414c84da6954744241dd1c5bacc0be355e
-size 68762
+oid sha256:f9b591f6b38374053930925725cd708a6a56fbc8b4cf0f57e92b935ff29f1582
+size 71326
